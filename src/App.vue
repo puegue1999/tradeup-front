1991913579a8b4f9246542e3ef7b9450423836d8
puegue1999 <ra-fasilva2010@hotmail.com>
2024-02-19 20:55:44 -0300

    feat: Chamada do Cep ao iniciar o programa

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Cep />
+</template>
+
+<script>
+import Cep from './main/Cep.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Cep
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
